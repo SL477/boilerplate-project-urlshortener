db5ff58f72433326962d0733a12a9a4e0b6d0db4
SL477 <fishwickt@gmail.com>
2019-06-17 21:20:13 +0100

    Started parsing post requests
    got post requests working

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
-
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function(req, res){
@@ -31,6 +32,11 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+//post urls
+app.post("/api/shorturl/new", function (req, res) {
+  console.log(req.body.url);
+  res.json({"original_url": req.body.url});
+});
 
 app.listen(port, function () {
   console.log('Node.js listening ...');
